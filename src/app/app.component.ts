48f7da784dd708e58efc3ae813f873432a281518
pengalaman-it <pengalaman.it@gmail.com>
2022-01-27 12:26:49 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import { Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <p>
+      Select a title to set on the current HTML document:
+    </p>
+
+    <ul>
+      <li><a (click)="setTitle('Good morning!')">Good morning</a>.</li>
+      <li><a (click)="setTitle('Good afternoon!')">Good afternoon</a>.</li>
+      <li><a (click)="setTitle('Good evening!')">Good evening</a>.</li>
+    </ul>
+  `,
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  // title = 'Testing Tittle Anggular Cuy';
+  public constructor(private titleService: Title) { }
+
+  public setTitle(newTitle: string) {
+    this.titleService.setTitle(newTitle);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
+import { NgModule } from '@angular/core';
+import { BrowserModule, Title } from '@angular/platform-browser';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
+  imports: [
+    BrowserModule
+  ],
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+  providers: [
+    Title
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
